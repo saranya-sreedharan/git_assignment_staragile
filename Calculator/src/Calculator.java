c3c78f67449a083ad30c51bce8df883f7c0015c6
Ubuntu <ubuntu@ip-172-31-4-90.us-east-2.compute.internal>
2023-07-04 01:42:37 +0000

    This is my first commit

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+public class Calculator {
+	
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+    
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        Calculator calculator = new Calculator();
+        System.out.println("Sum: " + calculator.sum(num1, num2));
+        System.out.println("Subtraction: " + calculator.subtract(num1, num2));
+        System.out.println("Multiplication: " + calculator.multiply(num1, num2));
+        System.out.println("Division: " + calculator.divide(num1, num2));
+
+        scanner.close();
+    }
+}
